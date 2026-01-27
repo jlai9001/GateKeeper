92cbb195e0041437e351fff8d4ad7e94be19df7a
Jonathan Lai <jlai9001@gmail.com>
2026-01-27 11:31:21 -0800

    navigation setup

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,10 +1,42 @@
 const express = require ('express');
 const router = express.Router();
 
-// Routes
+// Routes - Page Navigation
+
 router.get('',(req,res)=> {
     res.render('index');
 })
+
+router.get('/objective',(req,res)=> {
+    res.render('pages/objective');
+})
+
+router.get('/logic',(req,res)=> {
+    res.render('pages/logic');
+})
+
+router.get('/solar',(req,res)=> {
+    res.render('pages/solar');
+})
+
+router.get('/electronics',(req,res)=> {
+    res.render('pages/electronics');
+})
+
+router.get('/sensors',(req,res)=> {
+    res.render('pages/sensors');
+})
+
+router.get('/wiring',(req,res)=> {
+    res.render('pages/wiring');
+})
+
+router.get('/programming',(req,res)=> {
+    res.render('pages/programming');
+})
+
+
+
 
 // export router
 module.exports = router;
