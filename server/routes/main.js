954a5c8c3389a16de4cf67ee6232350ab3429964
Jonathan Lai <jlai9001@gmail.com>
2026-02-09 11:17:30 -0800

    drop down menu label dynamic

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,36 +7,36 @@ router.get('',(req,res)=> {
     res.render('index');
 })
 
-router.get('/objectives',(req,res)=> {
-    res.render('pages/objectives');
-})
+router.get('/objectives', (req, res) => {
+  res.render('pages/objectives', { navLabel: 'Objectives' });
+});
 
 router.get('/logic',(req,res)=> {
-    res.render('pages/logic');
+    res.render('pages/logic', { navLabel: 'Logic' });
 })
 
 router.get('/solar',(req,res)=> {
-    res.render('pages/solar');
+    res.render('pages/solar', { navLabel: 'Solar' });
 })
 
 router.get('/electronics',(req,res)=> {
-    res.render('pages/electronics');
+    res.render('pages/electronics', { navLabel: 'Electronics' });
 })
 
 router.get('/sensors',(req,res)=> {
-    res.render('pages/sensors');
+    res.render('pages/sensors', { navLabel: 'Sensors' });
 })
 
 router.get('/wiring',(req,res)=> {
-    res.render('pages/wiring');
+    res.render('pages/wiring', { navLabel: 'Wiring' });
 })
 
 router.get('/programming',(req,res)=> {
-    res.render('pages/programming');
+    res.render('pages/programming', { navLabel: 'Programming' });
 })
 
 router.get('/video',(req,res)=> {
-    res.render('pages/video');
+    res.render('pages/video', { navLabel: 'Video' });
 })
 
 
